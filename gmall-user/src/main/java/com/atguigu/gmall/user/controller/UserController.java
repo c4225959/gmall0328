9acf5fba8f58c97ae7e9d820f12da4d88ff25d55
xiaojiaojiao <jiao@atguigu.com>
2018-07-30 19:37:16 +0800

    my three commit!

--- gmall-user/src/main/java/com/atguigu/gmall/user/controller/UserController.java
+++ gmall-user/src/main/java/com/atguigu/gmall/user/controller/UserController.java
@@ -1,6 +1,6 @@
 package com.atguigu.gmall.user.controller;
 
-import com.atguigu.gmall.bean.UserInfo;
+import com.atguigu.gmall.bean.UserAddress;
 import com.atguigu.gmall.bean.UserInfo;
 import com.atguigu.gmall.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +8,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @RestController
@@ -18,10 +17,66 @@ public class UserController {
     UserService userService;
 
     @RequestMapping("userInfoList")
-    public ResponseEntity<List<UserInfo>> userInfoList(HttpServletRequest request){
+    public ResponseEntity<List<UserInfo>> userInfoList(){
 
         List<UserInfo> userInfoList = userService.userInfoList();
 
         return ResponseEntity.ok(userInfoList);
+    }
+
+    @RequestMapping("userInfo")
+    public ResponseEntity<UserInfo> queryUserInfo(String id){
+
+        UserInfo userInfo = userService.queryUserInfo(id);
+
+        return ResponseEntity.ok(userInfo);
+    }
+
+    @RequestMapping("updateUserInfo")
+    public ResponseEntity<Integer> updateUserInfo(UserInfo userInfo){
+        Integer result = userService.updateUserInfo(userInfo);
+        return ResponseEntity.ok(result);
+    }
+
+    @RequestMapping("deleteUserInfo")
+    public ResponseEntity<Integer> deleteUserInfo(String id){
+        Integer result = userService.deleteUserInfo(id);
+        return ResponseEntity.ok(result);
+    }
+
+    @RequestMapping("addUserInfo")
+    public ResponseEntity<Integer> addUserInfo(UserInfo userInfo){
+        Integer result = userService.addUserInfo(userInfo);
+        return ResponseEntity.ok(result);
+    }
+
+    @RequestMapping("userAddressList")
+    public ResponseEntity<List<UserAddress>> userAddressList(){
+        List<UserAddress> userAddressList = userService.userAddressList();
+        return ResponseEntity.ok(userAddressList);
+    }
+
+    @RequestMapping("getUserAddressByUserId")
+    public ResponseEntity<List<UserAddress>> getUserAddressByUserId(String userId){
+        List<UserAddress> userAddressList = userService.getUserAddressByUserId(userId);
+        return ResponseEntity.ok(userAddressList);
+    }
+
+    @RequestMapping("addUserAddress")
+    public ResponseEntity<Integer> addUserAddress(UserAddress userAddress){
+        Integer result = userService.addUserAddress(userAddress);
+        return ResponseEntity.ok(result);
+    }
+
+    @RequestMapping("updateUserAddress")
+    public ResponseEntity<Integer> updateUserAddress(UserAddress userAddress){
+        Integer result = userService.updateUserAddress(userAddress);
+        return ResponseEntity.ok(result);
+    }
+
+    @RequestMapping("deleteUserAddress")
+    public ResponseEntity<Integer> deleteUserAddress(String id){
+        Integer result = userService.deleteUserAddress(id);
+        return ResponseEntity.ok(result);
     }
 }
--- gmall-user/src/main/java/com/atguigu/gmall/user/service/impl/UserServiceImpl.java
+++ gmall-user/src/main/java/com/atguigu/gmall/user/service/impl/UserServiceImpl.java
@@ -1,8 +1,9 @@
 package com.atguigu.gmall.user.service.impl;
 
-import com.atguigu.gmall.bean.UserInfo;
+import com.atguigu.gmall.bean.UserAddress;
 import com.atguigu.gmall.bean.UserInfo;
 import com.atguigu.gmall.service.UserService;
+import com.atguigu.gmall.user.mapper.UserAddressMapper;
 import com.atguigu.gmall.user.mapper.UserInfoMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,8 +16,61 @@ public class UserServiceImpl implements UserService {
     @Autowired
     UserInfoMapper userInfoMapper;
 
+    @Autowired
+    UserAddressMapper userAddressMapper;
+
     @Override
     public List<UserInfo> userInfoList() {
         return userInfoMapper.selectAll();
     }
+
+    @Override
+    public UserInfo queryUserInfo(String id) {
+        return userInfoMapper.selectByPrimaryKey(id);
+    }
+
+    @Override
+    public Integer updateUserInfo(UserInfo userInfo) {
+
+        return userInfoMapper.updateByPrimaryKey(userInfo);
+    }
+
+    @Override
+    public Integer deleteUserInfo(String id) {
+        return userInfoMapper.deleteByPrimaryKey(id);
+    }
+
+    @Override
+    public Integer addUserInfo(UserInfo userInfo) {
+        return userInfoMapper.insert(userInfo);
+    }
+
+    @Override
+    public List<UserAddress> userAddressList() {
+        return userAddressMapper.selectAll();
+    }
+
+    @Override
+    public List<UserAddress> getUserAddressByUserId(String userId) {
+        UserAddress userAddress = new UserAddress();
+        userAddress.setUserId(userId);
+        return userAddressMapper.select(userAddress);
+    }
+
+    @Override
+    public Integer addUserAddress(UserAddress userAddress) {
+        return userAddressMapper.insert(userAddress);
+    }
+
+    @Override
+    public Integer updateUserAddress(UserAddress userAddress) {
+        return userAddressMapper.updateByPrimaryKey(userAddress);
+    }
+
+    @Override
+    public Integer deleteUserAddress(String id) {
+        return userAddressMapper.deleteByPrimaryKey(id);
+    }
+
+
 }
