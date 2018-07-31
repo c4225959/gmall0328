f84143178d07be3d8814345b8ff3cb3596eeb8e2
xiaojiaojiao <jiao@atguigu.com>
2018-07-31 11:25:58 +0800

    my three commit!

--- gmall-user/src/main/java/com/atguigu/gmall/user/GmallUserApplication.java
+++ gmall-user/src/main/java/com/atguigu/gmall/user/GmallUserApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
-@MapperScan("com.atguigu.gmall.user.mapper")
+@MapperScan(basePackages = "com.atguigu.gmall.user.mapper")
 public class GmallUserApplication {
 
 	public static void main(String[] args) {
